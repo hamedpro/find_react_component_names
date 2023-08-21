681c982e47e9679ad7f10ef0dde87817d8628660
hamed yaghoutpour <hamedpro30@gmail.com>
2023-08-21 11:37:04 +0330

    initial commit

---
+++ find_component_names.js
@@ -0,0 +1,30 @@
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+import cliProgress from "cli-progress";
+import util from "util";
+var files = execSync("git ls-tree -r main --name-only")
+	.toString()
+	.split("\n")
+	.filter((i) => i && i.endsWith(".jsx"));
+var component_names = [];
+const progress_bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+progress_bar.start(files.length, 0);
+files.forEach((filename, index) => {
+	var my_regex = /<(?<component_name>[A-Z]{1}[A-Za-z0-9]*)/g;
+	var regex_output = readFileSync(filename, "utf8").matchAll(my_regex);
+
+	for (var output of regex_output) {
+		component_names.push(output.groups?.component_name);
+	}
+
+	progress_bar.update(index + 1);
+});
+progress_bar.stop();
+
+console.log(
+	util.inspect(component_names, {
+		showHidden: false,
+		depth: null,
+		colors: true,
+	})
+);
